1004873db629d1f584beb6cb4e0262b6e3f7bc7c
hbrls <shuaizhexu@gmail.com>
2016-09-24 00:00:05 +0800

    feature: xhr2/get

---
+++ xhr2/get.js
@@ -0,0 +1,35 @@
+module.exports = function $get(endpoint, query) {
+  query = query || {};
+
+  var esc = encodeURIComponent;
+
+  var keys = Object.keys(query);
+
+  var url = endpoint;
+  if (keys.length > 0) {
+    url += '?' + keys.map(function (k) {
+      var value = query[k];
+      if (value == null || value == '') {
+        return '';
+      } else {
+        return esc(k) + '=' + esc(value);
+      }
+    }).join('&');
+  }
+
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+      // console.debug(xhr);
+      var status = xhr.status;
+      if (status === 200) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.response);
+      }
+    };
+    xhr.send();
+  });
+};
