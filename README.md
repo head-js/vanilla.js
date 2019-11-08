0c2043e22414e0f3d7f8229349d9a39682a96020
hbrls <shuaizhexu@gmail.com>
2019-11-08 23:30:57 +0800

    R: fetch/$post

--- README.md
+++ README.md
@@ -3,4 +3,15 @@ vanilla.js
 
 1. jquery
 
-2. dispatcher
+2. forge
+
+3. dispatcher
+
+
+CHANGELOG
+==
+
+v0.2.0
+--
+
+*BREAKING CHANGE:* fetch/$post `{ code, message, k: v }` -> `{ code, message, k: v, headers }`. It does not follow the `{ code, message, data: { k: v }, headers }` of [head-http](https://github.com/pro-web-ng/head-http). Serious users should change to [head-http](https://github.com/pro-web-ng/head-http) while `vanilla.js/fetch` left for simple cases.
--- fetch/post.js
+++ fetch/post.js
@@ -1,18 +1,32 @@
-module.exports = function $post(endpoint, command) {
+module.exports = function $post(endpoint, command, headers) {
   command = command || {};
+
+  headers = headers || {};
+  headers['Accept'] = 'application/json';
+  headers['Content-Type'] = 'application/json';
 
   return fetch(endpoint, {
     method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
+    headers: headers,
     credentials: 'same-origin',
     body: JSON.stringify(command),
   })
   .then(function (resp) {
     if (resp.status >= 200 && resp.status < 300) {
-      return resp.json().then(function (j) { return Promise.resolve(j); });
+      return resp.json().then(function (j) {
+        var hds = {};
+        var x = false;
+        for (var kv of resp.headers.entries()) {
+          if (kv[0].indexOf('x-') > -1) {
+            hds[kv[0]] = kv[1];
+            x = true;
+          }
+        }
+        if (x) {
+          j.headers = hds;
+        }
+        return Promise.resolve(j);
+      });
     } else {
       return resp.json().then(function (j) { return Promise.reject(j); });
     }
